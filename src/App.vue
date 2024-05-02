9c34972a9201b91a158ec018ea40360ff9d36e70
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-05-01 22:32:20 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,7 @@ function submitForm() {
         <p class="uppercase tracking-[.25rem] text-sm">35,000+ already joined</p>
         <h6 class="font-semibold text-2xl lg:text-3xl mt-3 mb-6">Stay up-to-date with what we’re doing</h6>
         <form @submit.prevent="onSubmit" class="flex flex-col w-full lg:flex-row lg:gap-x-2 lg:items-start">
-          <div class="relative mb-3" >
+          <div class="relative mb-3 w-full" >
             <label for="email" class="rounded-md lg:w-2/3" :class="[errorMsg ? 'border bg-PrimarySoftRed border-PrimarySoftRed': 'border-none']">
               <input 
                 id="email" 
