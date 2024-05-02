ebee084d561e09191bae757fb068b23818ad3f0c
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-05-01 22:38:52 -0400

    Update TheNavbar.vue

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -24,7 +24,7 @@ function menuOpen() {
     <div class="bg-white h-20 lg:h-32 fixed top-0 z-20 w-full lg:flex lg:justify-between lg:items-center lg:px-28 xl:px-32">
 
         <div class="py-7 px-8 lg:p-0">
-            <a href="#" class="absolute z-20 top-0 left-0 pt-7 lg:pt-9 lg:pl-24" :class="[open ? 'pl-12' : '']">
+            <a href="#" class="absolute z-20 top-0 left-0 pt-7 lg:pt-11 lg:pl-24" :class="[open ? 'pl-12' : '']">
               <img 
                 :src="[open ? LogoWhite : Logo]" 
                 alt="Logo" 
